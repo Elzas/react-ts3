0d354128adde42dc4f9314310a0dbef9bf158884
FCZLumpy <balytckyy@gmail.com>
2019-09-15 19:40:39 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Confirm from "./Confirm";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+    confirmOpen: boolean;
+    confirmMessage: string;
+    confirmVisible: boolean;
+    countDown: number;
+}
+
+class App extends Component<{}, IState> {
+
+    private timer: number = 0;
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            confirmMessage: "Please hit the confirm button",
+            confirmOpen: false,
+            confirmVisible: true,
+            countDown: 10
+        };
+    }
+
+    private handleTimerTick = () => {
+        this.setState({
+            confirmMessage: `Please hit the confirm button ${this.state.countDown} secs to go`,
+            countDown: this.state.countDown - 1
+        })
+    };
+
+    private handleCancelConfirmClick = () => {
+        this.setState({
+            confirmMessage: "Take a break, I'm sure you will later...",
+            confirmOpen: false
+        });
+    };
+
+    private handleOkConfirmClick = () => {
+        this.setState({
+            confirmMessage: "Cool, carry on reading!",
+            confirmOpen: false
+        });
+    };
+
+    private handleConfirmClick = () => {
+        this.setState({confirmOpen: true});
+    };
+
+    componentDidMount() {
+        this.timer = window.setInterval(() => this.handleTimerTick(),
+            1000);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+                <p>{this.state.confirmMessage}</p>
+                <button onClick={this.handleConfirmClick}>Confirm</button>
+                <Confirm
+                    open={this.state.confirmOpen}
+                    title="React and TypeScript"
+                    content="Are you sure you want to learn React and TypeScript?"
+                    cancelCaption="No way"
+                    okCaption="Yes please!"
+                    onCancelClick={this.handleCancelConfirmClick}
+                    onOkClick={this.handleOkConfirmClick}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
